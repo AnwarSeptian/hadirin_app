24348e02e1cf0c208c47df2c266ff9e290dd4d72
AnwarSeptian <anwarpalmer26@gmail.com>
2025-07-09 01:01:34 +0700

    add utils

---
+++ lib/utils/endpoint.dart
@@ -0,0 +1,6 @@
+class Endpoint {
+  static const String baseUrl = "https://appabsensi.mobileprojp.com/api";
+  static const String register = "$baseUrl/register";
+  static const String training = "$baseUrl/trainings";
+  static const String batch = "$baseUrl/batches";
+}
